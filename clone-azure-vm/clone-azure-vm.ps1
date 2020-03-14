073a1eb32122f8573083e40fb75ab4ad5af17028
Irfan95 <irfan.hassan@hotmail.co.uk>
2020-03-14 20:01:42 +0000

    Updated clone-azure-vm to place resources to new resource v1.1.1

--- clone-azure-vm/clone-azure-vm.ps1
+++ clone-azure-vm/clone-azure-vm.ps1
@@ -4,9 +4,33 @@
 #  Description  : Clone an existing Azure VM
 #  Author       : Irfan Hassan
 #  Date         : 14/03/2020
-#  Version      : 1.0.1
+#  Version      : 1.1.1
 #
 ##############################################################################################################
+
+## Prefix of the Existing VM
+$parentvmPrefix = ""
+
+## Uncomment the below if you wish to use a different location to the existing VM
+#$location = ""
+
+## Prefix of the New VM
+$vmPrefix = ""
+
+## Resource group name of new VM
+$newResourceGroupName = $vmPrefix + "-RG"
+
+## Enter the name of an existing virtual network where virtual machine will be created
+$virtualNetworkName = ""
+
+## Enter the name of the existing virtual networks resource group name
+$virtualNetworkRGName = ""
+
+## name of the newly created vm
+$virtualMachineName = $vmPrefix + "-vm"
+
+## ## Provide the size of the virtual machine e.g. "Standard_D2s_v3"
+$virtualMachineSize = ""
 
 ## Connect to Azure Account
 Connect-AzAccount
@@ -15,61 +39,74 @@ Connect-AzAccount
 $context = Get-AzSubscription -SubscriptionId ""
 Select-AzSubscription $context
 
-## Select existing disks by entering the prefix used for the disk names before the asterisk e.g. VMPrefix*
-$ExistingDisks = Get-AzDisk -Name *
+## Select existing disks
+$existingDisks = Get-AzDisk -Name $parentVMPrefix*
 
 ## Create a snapshot of each of the disks selected
-Foreach ($Disk in $ExistingDisks)
+Foreach ($disk in $existingDisks)
     {
-        $resourceGroupName = $Disk.ResourceGroupName
-        $location = $Disk.Location
-        $snapshotName = $Disk.name + "-Snapshot"
-
+        $parentResourceGroupName = $disk.ResourceGroupName
+        $snapshotName = $disk.name + "-Snapshot"
+        if (!$location)
+            {
+                $location = $disk.Location
+            }
         $snapshot = New-AzSnapshotConfig `
-        -SourceUri $Disk.Id `
-        -Location $Disk.Location `
+        -SourceUri $disk.Id `
+        -Location $location `
         -CreateOption copy
 
         New-AzSnapshot `
         -Snapshot $snapshot `
         -SnapshotName $snapshotName `
-        -ResourceGroupName $resourceGroupName
+        -ResourceGroupName $parentresourceGroupName
     }
 
-## Select newly created snapshots by entering the prefix used for the snapshots before the asterisk e.g. VMPrefix*
-$ExistingSnapshots = Get-AzSnapshot -SnapshotName *
+## Create the new Resource Group if it doesnt exist
+Get-AzResourceGroup -Name $newResourceGroupName -ErrorAction SilentlyContinue -ErrorVariable newRGError
+If ($newRGError)
+    {
+        New-AzResourceGroup -Name $newResourceGroupName -Location $location
+    }
+
+## Select newly created OS snapshot
+$osSnapshot = Get-AzSnapshot -SnapshotName $parentVMPrefix*OS*
+
+## Create a managed disk from the OS snapshot
+
+$osSnapshotName = $osSnapshot.Name
+$osDiskName = $vmPrefix + "-OSDisk1"
+$diskSize = $osSnapshot.DiskSizeGB
+$osStorageType = "Standard_LRS"
+$snapshot = Get-AzSnapshot -ResourceGroupName $resourceGroupName -SnapshotName $osSnapshotName
+$osDiskConfig = New-AzDiskConfig -Location $location -DiskSizeGB $diskSize -SkuName $osStorageType -CreateOption copy -SourceResourceId $snapshot.Id
+$osDisk = New-AzDisk -Disk $osDiskConfig -ResourceGroupName $newResourceGroupName -Diskname $osDiskName
+
+## Select newly created Data snapshots
+$dataSnapshots = Get-AzSnapshot -SnapshotName $parentVMPrefix*data*
+
+## Set the DataDisk No. to start from 0
+$DatadiskNo = 1
 
 ## Create a managed disk from each snapshot
-foreach ($snapshot in $ExistingSnapshots)
+foreach ($snapshot in $dataSnapshots)
     {
         $snapshotName = $snapshot.Name
-        $resourceGroupName = $snapshot.ResourceGroupName
-        
-        ## Change the replace variables to match your prefix
-        $diskName1 = $snapshotName -replace ("","")
-        $diskName = $diskName1 -replace ("-Snapshot","")
-        
+        $diskName = $vmPrefix + "-DataDisk" + $DatadiskNo
+        $diskSize = $snapshot.DiskSizeGB
         $storageType = "Standard_LRS"
-        $location = $snapshot.Location
-        $snapshot = Get-AzSnapshot -ResourceGroupName $resourceGroupName -SnapshotName $snapshotName
-        $diskConfig = New-AzDiskConfig -Location $location -SkuName $storageType -CreateOption copy -SourceResourceId $snapshot.Id
-        $disk = New-AzDisk -Disk $diskConfig -ResourceGroupName $resourceGroupName -Diskname $diskName
+        $snapshot = Get-AzSnapshot -ResourceGroupName $parentResourceGroupName -SnapshotName $snapshotName
+        $diskConfig = New-AzDiskConfig -Location $location -DiskSizeGB $diskSize -SkuName $storageType -CreateOption copy -SourceResourceId $snapshot.Id
+        $disk = New-AzDisk -Disk $diskConfig -ResourceGroupName $newResourceGroupName -Diskname $diskName
+        $DatadiskNo++
     }
-
-## Enter the name of an existing virtual network where virtual machine will be created
-$virtualNetworkName = ""
-
-## Enter the name of the virtual machine to be created
-$virtualMachineName = ""
-
-## Provide the size of the virtual machine e.g. "Standard_D2s_v3"
-$virtualMachineSize = ""
 
 ## Initialize virtual machine configuration
 $VirtualMachine = New-AzVMConfig -VMName $virtualMachineName -VMSize $virtualMachineSize
 
-## Select the newly created Datadisks by entering the prefix used for the disk names before the asterisk e.g. VMPrefix*
-$NewDataDisks = Get-AzDisk -Name *
+## Select the newly created Datadisks
+$vmDataDiskName = $vmPrefix + "-DataDisk"
+$NewDataDisks = Get-AzDisk -Name $vmDataDiskName*
 
 ## Set the LUN to start from 0
 $LUN = 0
@@ -81,22 +118,22 @@ foreach ($DataDisk in $NewDataDisks)
         $LUN++
     }
 
-## Select newly created snapshots by entering the prefix used for the snapshots before the asterisk e.g. VMPrefix*
-$OSDisk = Get-AzDisk -Name * 
+$vmOSDiskName = $vmPrefix + "-OS"
+$osDisk = Get-AzDisk -Name $vmOSDiskName* 
 
 #Use the Managed Disk Resource Id to attach it to the virtual machine. Use OS type based on the OS present in the disk - Windows / Linux
-$VirtualMachine = Set-AzVMOSDisk -VM $VirtualMachine -ManagedDiskId $OSDisk.Id -CreateOption Attach -Windows
+$VirtualMachine = Set-AzVMOSDisk -VM $VirtualMachine -ManagedDiskId $osDisk.Id -CreateOption Attach -Windows
 
 #Create a public IP 
-$publicIp = New-AzPublicIpAddress -Name ($VirtualMachineName.ToLower()+'_ip') -ResourceGroupName $resourceGroupName -Location $snapshot.Location -AllocationMethod Dynamic
+$publicIp = New-AzPublicIpAddress -Name ($VirtualMachineName.ToLower()+'_ip') -ResourceGroupName $newResourceGroupName -Location $Location -AllocationMethod Dynamic
 
 #Get VNET Information
-$vnet = Get-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $virtualNetworkName
+$vnet = Get-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $virtualNetworkRGName
 
 # Create NIC for the VM
-$nic = New-AzNetworkInterface -Name ($VirtualMachineName.ToLower()+'_nic') -ResourceGroupName $resourceGroupName -Location $snapshot.Location -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $publicIp.Id
+$nic = New-AzNetworkInterface -Name ($VirtualMachineName.ToLower()+'_nic') -ResourceGroupName $newResourceGroupName -Location $Location -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $publicIp.Id
 
 $VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $nic.Id
 
 #Create the virtual machine with Managed Disk
-New-AzVM -VM $VirtualMachine -ResourceGroupName $resourceGroupName -Location $snapshot.Location
+New-AzVM -VM $VirtualMachine -ResourceGroupName $newResourceGroupName -Location $Location
--- clone-azure-vm/readme.md
+++ clone-azure-vm/readme.md
@@ -1,29 +1,38 @@
-# Readme
+## Readme
 This script will create clone disks of the selected parent VM and generate a VM with those newly cloned disks.
 
 ---
 
-# CHANGELOG
+## Changelog
 Below you will find changes made along with new issues found in each version.
 
 ---
 
-## [v.1.0.0] 13/03/2020
-### Added
-- Created azure-clone-vm.ps1
+### [v.1.0.0] 13/03/2020
+#### Added
+- Created azure-clone-vm.ps1.
 
-### Known Issues
-- [#1] Resources dont always get discovered (ln:19) (ln:40) (ln:72) (ln:85)
+#### Known Issues
+- [#1] Resources dont always get discovered. (ln:19) (ln:40) (ln:72) (ln:85)
 - [#2] Timeout when attempting to contact vm extensions once vm is created. (ln:102)
-- [#3] NSG doesnt get created.
+- [#3] NSG is not attached/created.
 
 ---
 
-## [v1.0.1] 14/03/2020
-### Removed
-- Removed quotes from (ln:19) (ln:40) (ln:72) (ln:85) to improve discovery of resources
+### [v1.0.1] 14/03/2020
+#### Removed
+- Removed quotes from (ln:19) (ln:40) (ln:72) (ln:85) to improve discovery of resources.
 
-### Resolved
-- [#1] Resource discovery has been improved with the removed quotes
+#### Resolved
+- [#1] Resource discovery has been improved with the removed quotes.
+
+---
+
+### [v1.1.1] 14/03/2020
+#### Added
+- Added variable to create and store vm in new Resource Group.
+- Create new Resource Group if it doesnt exist.
+- Moved all variables to top of script to make modifications easier.
+- Added prefix variable to name new resources more consistently.
 
 ---
